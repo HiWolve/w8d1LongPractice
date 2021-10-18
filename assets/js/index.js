a3fcf47e46ca5c8c7e6394764830ab77297e1a91
Kreston <kreston.cm@gmail.com>
2021-10-18 18:59:40 -0500

    Phase 2 complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
-changeTitle();
-changeHeader();
-changeAboutMe();
-console.log(findElementById("header")); // <div id="header">
-console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+import * as Man from "/assets/js/manipulate.js"
+import * as Search from "/assets/js/search.js"
+
+window.onload = () => {
+  Man.changeTitle();
+  Man.changeHeader();
+  Man.changeAboutMe();
+  console.log(Search.findElementById("header")); // <div id="header">
+  console.log(Search.findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
+  console.log(Search.findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
+  console.log(Search.findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+  console.log(Search.findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+}
